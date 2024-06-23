ccd839d59bd8b6b6ae66ca7864c99091a2cfa9de
idimitrisc47 <dimitrisc47@gmail.com>
2024-06-23 18:28:31 -0500

    Criando o Grid 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import logoImage from './assets/devmemory_logo.png';
 import RestartIcon from './svgs/restart.svg';
 import { InfoItem } from './components/infoItem';
 import { Button } from './components/button';
+import { useEffect, useState } from 'react';
+import { GridItemType } from './types/gridItemType';
 const App = () => {
+  const [playing,setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount,setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+  useEffect(() => resetAndCreateGrid(), []);
+
   const resetAndCreateGrid = () => {
 
   }
@@ -12,16 +21,18 @@ const App = () => {
       <C.Container>
         <C.Info>
           <C.LogoLink href="">
-              <img src={logoImage} width="200" alt=""></img>
+            <img src={logoImage} width="200" alt=""></img>
           </C.LogoLink>
           <C.InfoArea>
-            <InfoItem label= "Tempo" value="00:00"/>
-            <InfoItem label= "Movimentos" value="0"/>
+            <InfoItem label="Tempo" value="00:00" />
+            <InfoItem label="Movimentos" value="0" />
           </C.InfoArea>
-          <Button icon={RestartIcon} label='Reiniciar' onClick={resetAndCreateGrid}/>
+          <Button icon={RestartIcon} label='Reiniciar' onClick={resetAndCreateGrid} />
         </C.Info>
         <C.GridArea>
-            ....
+          <C.Grid>
+
+          </C.Grid>
         </C.GridArea>
       </C.Container>
     </div>
